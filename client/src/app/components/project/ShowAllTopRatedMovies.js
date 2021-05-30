1602b335c3d56f2009f34394759d2c1b9624118e
pgm-dieterschepers <dieter.schepers@student.arteveldehs.be>
2021-05-30 17:45:09 +0200

    added functional search field for movies

--- client/src/app/components/project/ShowAllTopRatedMovies.js
+++ client/src/app/components/project/ShowAllTopRatedMovies.js
@@ -1,48 +1,76 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import styles from './ShowAllMovies.module.scss';
-import ShowMovieGenres from './ShowMovieGenres';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { useState, useEffect } from "react";
+import styles from "./ShowAllMovies.module.scss";
+import ShowMovieGenres from "./ShowMovieGenres";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const ShowAllPopularMovies = (props) => {
   const key = "7598462be8b94fc1e04d0e6dd30a782e";
 
-  const [movies, setMovies] = useState([])
-
+  const [movies, setMovies] = useState([]);
+  const [searchterm, setSearchterm] = useState(null);
 
   const fetchPopularMovies = async () => {
-    const data = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`)
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`
+    );
     const items = await data.json();
-    setMovies(items.results)
-  }
+    setMovies(items.results);
+  };
   useEffect(() => {
     fetchPopularMovies();
-  }, [])
+  }, []);
 
   const StyledLink = styled(Link)`
     text-decoration: none;
 
-    &:focus, &:hover, &:visited, &:link, &:active {
-        text-decoration: none;
+    &:focus,
+    &:hover,
+    &:visited,
+    &:link,
+    &:active {
+      text-decoration: none;
     }
-`;
+  `;
 
   return (
-    <div className={styles.allPopularMovies}>
-      {movies.map((m) =>
-        <div className={styles.allPopularMoviesMovie} >
-          <StyledLink to={`movies/${m.id}`} key={m.id}>
-            <img src={`https://image.tmdb.org/t/p/original/${m.poster_path}`} alt={m.original_title} loading='lazy'/>
-            <p class={styles.originalTitle}>{m.original_title}</p>
-          </StyledLink>
-          <div className={styles.genreLinkWrap}>
-            {m.genre_ids.map(g => <ShowMovieGenres genreId={g} />)}
-          </div>
-        </div>
-      )}
-    </div>
-  )
-}
+    <div className={styles.searchMoviesWrap}>
+      <input className={styles.inputSearchField} type="text" placeholder='Search...' onChange={(e) => setSearchterm(e.target.value)} />
 
-export default ShowAllPopularMovies
+      <div className={styles.allPopularMovies}>
+        {movies
+          .filter((value) => {
+            if (searchterm === null) {
+              return value;
+            } else if (
+              value.original_title
+                .toLowerCase()
+                .includes(searchterm.toLowerCase())
+            ) {
+              return value;
+            }
+          })
+          .map((m) => (
+            <div className={styles.allPopularMoviesMovie}>
+              <StyledLink to={`movies/${m.id}`} key={m.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${m.poster_path}`}
+                  alt={m.original_title}
+                  loading="lazy"
+                />
+                <p class={styles.originalTitle}>{m.original_title}</p>
+              </StyledLink>
+              <div className={styles.genreLinkWrap}>
+                {m.genre_ids.map((g) => (
+                  <ShowMovieGenres genreId={g} />
+                ))}
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default ShowAllPopularMovies;
