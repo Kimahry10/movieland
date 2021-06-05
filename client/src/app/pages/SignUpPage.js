28c3d07a237bf97fe802c248a597f8de284f4c98
pgm-dieterschepers <dieter.schepers@student.arteveldehs.be>
2021-06-05 16:38:39 +0200

    styled signup form

--- client/src/app/pages/SignUpPage.js
+++ client/src/app/pages/SignUpPage.js
@@ -1,72 +1,99 @@
 import React, { useState, useEffect } from 'react'
 import { BaseLayout } from '../layouts'
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import firebase, { db, auth } from '../utilities/firebase';
 import { useAuth } from "../contexts/firebase/auth.context";
 import styled from 'styled-components';
+import styles from './SignUpPage.module.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const SignUpPage = () => {
 
-  const [user, setUser] = useState()
+  const { currentUser, signOut } = useAuth();
+
+  console.log(currentUser)
+
+
+  const [username, setUsername] = useState()
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
+  const [confirmPassword, setConfirmPassword] = useState()
 
   const [emailError, setemailError] = useState()
   const [passwordError, setPasswordError] = useState()
+  const [confirmPasswordError, setconfirmPasswordError] = useState()
 
-  const handleLogin = () => {
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch(err => {
-        switch (err.code) {
-          case "auth/invalid-email":
-          case "auth/user-disabled":
-          case  "auth/user-not-found":
-            setemailError(err.message)
-            break;
-          case "auth/wrong-password": 
-          setPasswordError(err.message);
-          break;
-      }
-      })
+  const handleSignUp = (e) => {
+    if (password === confirmPassword) {
+      e.preventDefault();
+      auth.createUserWithEmailAndPassword(email, password)
+        // console.log(email, password)
+        .catch(err => {
+          switch (err.code) {
+            case "auth/email-already-used":
+            case "auth/invalid-email":
+              setemailError(err.message)
+              break;
+            case "auth/weak-password":
+              setPasswordError(err.message);
+              break;
+          }
+        })
+    } else {
+      e.preventDefault();
+      setconfirmPasswordError('passwords do not match.')
+    }
+
   }
 
-  const handleSignUp = () => {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      // console.log(email, password)
-      .catch(err => {
-        switch (err.code) {
-          case "auth/email-already-used":
-          case "auth/invalid-email":
-            setemailError(err.message)
-            break;
-          case "auth/weak-password":
-            setPasswordError(err.message);
-            break;
-        }
-      })
-  }
-
-const Error = styled.p `
+  const Error = styled.p`
   color: red;
   font-size: 1rem;
 `
 
+  if (currentUser) {
+    return <Redirect to='/' />
+  } else {
+    return (
+      <BaseLayout>
+        <div className="page page--sign-up">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6 ">
+                <form onSubmit={handleSignUp} className='d-flex flex-column'>
+                  <div className="form-group">
+                    <label className='w-100' htmlFor="email">
+                      Email
+                      <input className="form-control" type="text" name='email' id='email' required value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </label>
+                    <Error>{emailError}</Error>
+                  </div>
 
-  return (
-    <BaseLayout>
-      <label htmlFor="">Email</label>
-      <input type="text" required value={email} onChange={(e) => setEmail(e.target.value) } />
-      <label htmlFor="password">password</label>
-      <Error>{emailError}</Error>
-      <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleSignUp}>sign up</button>
-      <Error>{passwordError}</Error>
-    </BaseLayout>
-  )
+                  <div className="form-group">
+                    <label className='w-100'  htmlFor="password">
+                      password
+                    <input className="form-control" type="password" name='password' id='password' required value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </label>
+                    <Error>{passwordError}</Error>
+                  </div>
+                  <div className="form-group">
+                    <label className='w-100'  htmlFor="confirmPassword">
+                      confirm password
+        <input className="form-control" type="password" name='confirmPassword' id='confirmPassword' required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                    </label>
+                    <Error>{confirmPasswordError}</Error>
+                  </div>
+                  <input className="btn btn-primary w-100" type="submit" value="sign up" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </BaseLayout>
+    )
+  }
+
 }
 
 export default SignUpPage
